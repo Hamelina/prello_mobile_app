6e6944fac39e07859e22a84263cf80cfd5907d86
assilelyahyaoui <elyahyaouiassil@hotmail.com>
2018-11-02 18:19:24 +0100

    Switching to caroussel

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,9 +1,10 @@
 
 import React , {Component} from 'react';
 import { connect } from 'react-redux'; 
-import { View , Text ,Platform,StyleSheet,Dimensions} from 'react-native';
-
+import { View , Text ,Platform,StyleSheet,Dimensions, ScrollView} from 'react-native';
+import Carousel from 'react-native-snap-carousel';
 import { CardViewWithIcon } from "react-native-simple-card-view";
+//import {SCREEN_HEIGHT , SCREEN_WIDTH} from "../constants"
 
 
 const
@@ -50,6 +51,15 @@ let json = [
         "dueDate": "Date",
         "done": "doing" ,
         "desc": "Desc Card 2 llelele"
+    },
+    {
+        "id": 3,
+        "name": "Card 3",
+        "idBoard": "1",
+        "idList": "2",
+        "dueDate": "Date",
+        "done": "doing" ,
+        "desc": "Desc Card 3 fit motha"
     }
 ]
 
@@ -69,52 +79,50 @@ class Cards extends Component {
         });
     }
     
+    _renderItem ({item, index}) {
+        const styles = StyleSheet.create({container: {alignItems: 'center',backgroundColor: '#F5FCFF',},});
+
+        return (
+            <View style={styles.container}>
+                <Text >{ item.id }</Text>
+                <Text >{ item.desc }</Text>
+            </View>
+        );
+    }
     
     render() {
-        const miniCardStyle = {shadowColor: '#000000',shadowOffsetWidth : 2,shadowOffsetHeight: 2,shadowOpacity     : 0.1,hadowRadius      : 5,bgColor           : '#ffffff',padding           : 5,margin            : 5,borderRadius      : 3,elevation         : 3,width             : (Dimensions.get("window").width / 2) - 10
-    };
-    const styles = StyleSheet.create({container: {flex           : 2, alignItems     : 'center',backgroundColor: '#F5FCFF',paddingTop     : 25,},});
-    
-    const cardsView =  json.map((cardDetail , index)=>{
-        return (
-            <View key = {index}>
-            <CardViewWithIcon
-            withBackground={ false }
-            // androidIcon={ 'logo-github' }
-            // iosIcon={ 'logo-github' }
-            iconHeight={ 0 }
-            // iconColor={ '#333' }
-            title={cardDetail.name}  
-            contentFontSize={ 20 }
-            titleFontSize={ 25 }
-            style={ miniCardStyle }
-            content={ cardDetail.desc }
-            // onPress={ () => this.setState({
-            //     github       : this.state.github + 1
-            // }) }
-            />
-            </View>
-            )
-        })
-        return(
-            <View>
-            {cardsView} 
-            </View>
-            )
-            
-        } 
+        const miniCardStyle = {shadowColor: '#000000',shadowOffsetWidth : 2,shadowOffsetHeight: 2,shadowOpacity: 0.1,hadowRadius: 5,bgColor: '#ffffff',
+        padding: 5,margin: 5,borderRadius: 3,elevation: 3, width : (Dimensions.get("window").width / 2) - 10};
         
-    }
-    const mapStateToProps = (state, props) => ({
-        desc: state.card.desc,
-        state: state.card.state
-        // TODO: Add store state to the component props
-    })
+        const styles = StyleSheet.create({container: {alignItems: 'center',backgroundColor: '#F5FCFF',paddingTop     : 25,},});
+        const screenwidth = Dimensions.get("window").width ;
+        const screenheight = Dimensions.get("window").height ;
+
     
-    const mapDispatchToProps = (dispatch, props) => ({
-        //setCheckCardState: (complete) => dispatch(setCheckCardState( props.id, complete ))
-        changeCardDesc: (event) => dispatch(changeCardDesc(props.id, event.target.value))
-        // TODO: Add 
-    })
-    
-    export default connect(mapStateToProps, mapDispatchToProps)(Cards); 
+            return(
+                <Carousel style = {miniCardStyle}
+                ref={(c) => { this._carousel = c; }}
+                data={json}
+                renderItem={this._renderItem}
+                sliderWidth={400}
+                itemWidth={screenwidth*0.75}
+                itemHeight= {1200}
+                />
+                )
+                
+            } 
+            
+        }
+        const mapStateToProps = (state, props) => ({
+            desc: state.card.desc,
+            state: state.card.state
+            // TODO: Add store state to the component props
+        })
+        
+        const mapDispatchToProps = (dispatch, props) => ({
+            //setCheckCardState: (complete) => dispatch(setCheckCardState( props.id, complete ))
+            changeCardDesc: (event) => dispatch(changeCardDesc(props.id, event.target.value))
+            // TODO: Add 
+        })
+        
+        export default connect(mapStateToProps, mapDispatchToProps)(Cards); 
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,5 @@
 import { Constants } from 'expo';
 
 export const STATUS_BAR_HEIGHT = Constants.statusBarHeight;
+// export const SCREEN_HEIGHT = Dimensions.get("window").height; 
+// export const SCREEN_WIDTH = Dimensions.get("window").width;
